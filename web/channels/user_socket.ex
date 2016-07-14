6c4e00e916f0adaff79a8907e78ef77011567c92
Ben Thomas <fearlessleaderthomas@gmail.com>
2016-07-14 01:31:51 -0400

    Different debugging

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -22,7 +22,6 @@ defmodule IronfireServer.UserSocket do
   # See `Phoenix.Token` documentation for examples in
   # performing token verification on connect.
   def connect(_params, socket) do
-    IO.puts inspect socket 
     {:ok, socket}
   end
 
